74d3143f998a498894eb6a0f70544176db9bf92d
William RAD Funk <contact@WILLIAMRADFunk.com>
2018-03-24 21:28:10 -0400

    Resolves some old copy paste errors.

--- src/app/components/profit-portfolio/profit-portfolio.component.ts
+++ src/app/components/profit-portfolio/profit-portfolio.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
 
+import { Chart } from 'angular-highcharts';
+
 @Component({
   selector: 'app-profit-portfolio',
   templateUrl: './profit-portfolio.component.html',
   styleUrls: ['./profit-portfolio.component.scss']
 })
 export class ProfitPortfolioComponent implements OnInit {
+  chart: Chart;
   pathState: string = 'BTC-USD';
 
   constructor(
@@ -18,5 +21,20 @@ export class ProfitPortfolioComponent implements OnInit {
       .subscribe((segments: UrlSegment[]) => {
         this.pathState = segments[0]['path'];
       });
+    this.chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: 'BTC-USD'
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'BTC-USD',
+        data: [1, 2, 3]
+      }]
+    });
   }
 }
