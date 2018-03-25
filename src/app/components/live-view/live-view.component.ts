0fa93e0e7050847ece209893f3666ce3f5c34552
William RAD Funk <contact@WILLIAMRADFunk.com>
2018-03-25 01:53:01 -0400

    Starts to dig deeper into available functioanlity with highcharts in regards to the live-view.

--- src/app/components/live-view/live-view.component.ts
+++ src/app/components/live-view/live-view.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
-import { Chart } from 'angular-highcharts';
+import { Chart, Highcharts } from 'angular-highcharts';
 
 import { GdaxDataService } from '../../services/gdax-data.service';
 
@@ -30,21 +30,43 @@ export class LiveViewComponent implements OnInit {
         console.log(data);
         this.chart = new Chart({
           chart: {
-            type: 'line'
+            type: 'line',
+            backgroundColor: 'rgba(255, 255, 255, 0)'
           },
           title: {
-            text: 'BTC-USD'
+            text: this.pathState
           },
           credits: {
             enabled: false
           },
           series: [{
-            name: 'BTC-USD',
+            name: this.pathState,
+            color: '#2d2d31',
             data: []
-          }]
+          }],
+          xAxis: {
+            title: {
+              text: null
+            }
+          },
+          yAxis: {
+            title: {
+              text: null
+            }
+          }
         });
+        const dps = [];
         data.forEach(element => {
-          this.chart.addPoint(element[4]);
+          const dp = {
+            value: element[4],
+            x: new Date(element[0] * 1000),
+            y: element[4],
+          };
+          dps.push(dp);
+        });
+        dps.reverse();
+        dps.forEach(element => {
+          this.chart.addPoint(element);
         });
       });
   }
--- src/app/services/gdax-data.service.ts
+++ src/app/services/gdax-data.service.ts
@@ -15,9 +15,7 @@ export class GdaxDataService {
       .set('Accept', 'application/json')
       .set('Content-Type', 'application/json');
     const params = new HttpParams()
-      .set('granularity', '86400')
-      .set('start', '2018-03-24T00:00:00Z')
-      .set('start', '2018-03-24T11:59:59Z');
+      .set('granularity', '300');
     return this.http.get<any>(`https://api.gdax.com/products/${currency}/candles`, {headers, params});
   }
 
